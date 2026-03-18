07ae17dea2e3489ac577e59412a0a1b7b4b60b7e
Kirill Tirskikh <director@academy-wincode.com>
2026-03-18 13:24:48 +0800

    исправление: цепочка миграций (003 ссылался на удалённую 002)
    Миграция 002 была исключена из репозитория (файл в корне проекта
    попал в .gitignore), что ломало цепочку Alembic при свежем деплое.
    003 теперь ссылается напрямую на 001.

--- backend/alembic/versions/003_course_hierarchy.py
+++ backend/alembic/versions/003_course_hierarchy.py
@@ -1,7 +1,7 @@
 """Course hierarchy: CourseNode, TopicStep, user progress
 
 Revision ID: 003
-Revises: 002
+Revises: 001
 Create Date: 2026-03-15
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = "003"
-down_revision: Union[str, None] = "002"
+down_revision: Union[str, None] = "001"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
